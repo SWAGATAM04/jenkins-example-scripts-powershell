91dcd3b9a34ca6dc11c823f0733e434a2c4dec5e
SWAGATAM04 <skundu012@gmail.com>
2022-11-24 16:47:23 +0530

    Add files via upload

---
+++ script.ps1
@@ -0,0 +1,45 @@
+
+echo "Checking IIS Server Status"
+
+echo "Checking if IIS is running"
+
+$serverList = 'localhost'
+
+foreach ($server in $serverList) {
+
+    $iis = Get-WmiObject Win32_Service -Filter "Name = 'IISADMIN'" -ComputerName $server
+
+    if ($iis.State -eq 'Running') { Write-Host "IIS is running on $server" }
+    Else { Write-Host "IIS is NOT running on $server" -ForegroundColor Red }
+
+}
+
+
+echo "MSSQL server is running"
+
+
+$server = $env:computername 
+
+$object = Get-service -ComputerName $server  | where {($_.name -like "MSSQL$*" -or $_.name -like "MSSQLSERVER" -or $_.name -like "SQL Server (*") }
+
+if ($object)
+
+{
+
+$instDetails= $object |select Name,Status
+
+$instDetails
+
+}else
+
+{
+
+Write-Host "0 SQL Server instances discovered"
+
+}
+
+echo "Checking if Tomcat port is opened?"
+netstat -ano | findstr 8080 |findstr LISTENING
+
+
+
