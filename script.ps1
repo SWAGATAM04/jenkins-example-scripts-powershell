2d4dbac9751f3b95410f7a9c5fa7390e533cb336
SWAGATAM04 <skundu012@gmail.com>
2022-11-26 19:54:24 +0530

    Update script.ps1

--- script.ps1
+++ script.ps1
@@ -7,7 +7,7 @@ $serverList = 'localhost'
 
 foreach ($server in $serverList) {
 
-    $iis = Get-WmiObject Win32_Service -Filter "Name = 'IISADMIN'" -ComputerName $server
+    $iis = Get-WmiObject Win32_Service -Filter "Name = 'W3SVC'" -ComputerName $server
 
     if ($iis.State -eq 'Running') { Write-Host "IIS is running on $server" }
     Else { Write-Host "IIS is NOT running on $server" -ForegroundColor Red }
